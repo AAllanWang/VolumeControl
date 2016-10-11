0415dc914501486a4a44b0fe5ce0e3ba67376f3a
U-ERICSSON\ewawenl <ewawenl@CN00119523.ericsson.se>
2016-10-11 15:02:56 +0800

    Init repo for my android phone volume control

---
+++ app/src/main/java/com/example/ewawenl/volumecontrol/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.ewawenl.volumecontrol;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.DragEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        curVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        sb = (SeekBar) findViewById(R.id.seekBar);
+        sb.setOnSeekBarChangeListener(this);
+        sb.setMax(maxVolume);
+        sb.setProgress(curVolume);
+        tv = (TextView) findViewById(R.id.textViewValue);
+        tv.setText("Max:" + maxVolume + " Current: " + curVolume );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        else if (id == R.id.action_exit) {
+            System.exit(0);
+            return true;
+        }
+        else if(id == R.id.action_info){
+            Toast.makeText(getApplicationContext(), "Developed by Wang Wenlong \n E-mail:lonwon@126.com",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        curVolume = seekBar.getProgress();
+        tv.setText("Current: " + curVolume);
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,curVolume,AudioManager.FLAG_PLAY_SOUND);
+    }
+
+    private AudioManager audioManager;
+    private SeekBar sb;
+    TextView tv;
+    private int maxVolume;
+    private int curVolume;
+}
